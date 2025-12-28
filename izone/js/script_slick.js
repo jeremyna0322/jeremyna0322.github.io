1c6ddf06c923dc91dd96fcfd3c9eba91a19d698b
jeremyna0322 <jeremyna0322@gmail.com>
2025-12-28 11:00:05 +0900

    Add files via upload

---
+++ izone/js/script_slick.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+$('.slider').slick({autoplay:true,
+autoplaySpeed:1200,
+dots:true
+});
+});
---
+++ izone/js/script_veno.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+  $('.imglist a').venobox({overlayColor:'#ff50a0'});
+});
